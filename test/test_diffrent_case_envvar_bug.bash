b121721419e1883f07b8d9f56e0c1b627c7c8096
Takaya Saeki <abc.tkys+pub@gmail.com>
2019-09-03 00:57:26 +0900

    Fix test cases

--- test/test_diffrent_case_envvar_bug.bash
+++ test/test_diffrent_case_envvar_bug.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source ./setup_test.bash
-tap_tests 10
+tap_tests 12
 
 export DIFFCASE1=FOO
 [[ $(sw echo %DiffCase1%) =~ FOO ]]; tap_okif $? "Test a variable of different cases can be read in Windows 1"
@@ -18,7 +18,9 @@ sw set_diff_case_env.cmd
 [[ "$DiffCase3" =~ BAR ]]; tap_okif $? "Test a variable of different cases is modified in Windows 3"
 [[ -z "$DIFFCASE3" ]]; tap_okif $? "Test a variable is undefined 3"
 
+unset DiffCase3
 export DIFFCASE3=BAZ
-sw set_diff_case_env.cmd
-[[ "$DIFFCASE3" =~ BAZ ]]; tap_okif $? "Test a variable of different cases merges 1"
+[[ $(sw echo %DiffCase3%) =~ BAZ ]]; tap_okif $? "Test a variable of different cases merges 1"
+[[ $(echo $DIFFCASE3) =~ BAZ ]]; tap_okif $? "Test a variable of different cases merges 2"
+[[ -z "$DiffCase3" ]]; tap_okif $? "Test a variable of different cases merges 3"
 
